1cf1775eb14fef847c5c0163c62ae3bf328c631b
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2025-04-04 12:45:31 +0530

    Enhance CORS configuration to specify allowed origins, methods, and headers

--- outflo-backend/src/app.ts
+++ outflo-backend/src/app.ts
@@ -12,7 +12,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://out-flo-assignment-kappa.vercel.app",
+      "http://localhost:5173", // Vite's default development port
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // Routes
